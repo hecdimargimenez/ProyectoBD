f7cc19034484463aa889b6199aa322e52e1e90a0
hecdimargimenez <hecdimargimenez@gmail.com>
2022-04-11 00:09:30 -0400

    Creando modelo y registro de App en settings

--- SistemaGarantiaApp/models.py
+++ SistemaGarantiaApp/models.py
@@ -1,5 +1,9 @@
+from faulthandler import cancel_dump_traceback_later
+from http.client import REQUESTED_RANGE_NOT_SATISFIABLE
 from ssl import create_default_context
+from django.conf import ENVIRONMENT_VARIABLE
 from django.db import models
+from numpy import real_if_close
 
 # Create your models here.
 
@@ -9,8 +13,18 @@ class Ticket(models.Model):
     cod_departamento=models.CharField(max_length=20)
     created=models.DateTimeField(auto_now_add=True)
     updated=models.DateTimeField(auto_now_add=True)
-    tipo_falla=models.CharField(max_length=20)
-    detalle_falla=models.CharField(max_length=20)
+    tipo_falla=models.CharField(max_length=50)
+    detalle_falla=models.CharField(max_length=300)
     arch_factura=models.FileField()
     cert_garantia=models.FileField()
+    estatus=models.BooleanField(default=True)
+
+
+class Cliente(models.Model):
+    cedula=models.CharField(max_length=15, primary_key=True)
+    rif=models.CharField(max_length=15)
+    nombre=models.CharField(max_length=50)
+    apellido=models.CharField(max_length=50)
+    email=models.EmailField(max_length=200)
+    telefono=models.CharField(max_length=20)
     estatus=models.BooleanField(default=True)
