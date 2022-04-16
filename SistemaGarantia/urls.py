fa6952759b5377b2c98c39482180866a4b138723
Sarahi Abigail <66806911+SarahiAbigail@users.noreply.github.com>
2022-04-16 00:23:56 -0400

    Se crean las views y urls

--- SistemaGarantia/urls.py
+++ SistemaGarantia/urls.py
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import views
 from django.contrib import admin
 from django.urls import path
-
+from SistemaGarantiaApp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name="Home"),
+    path('nosotros', views.nosotros, name="Nosotros"),
+    path('productos', views.productos, name="Productos"),
+    path('contacto', views.contacto, name="Contacto"),
+    path('reclamos', views.reclamos, name="Reclamos"),
+
+
+
+    
+
 ]
--- SistemaGarantiaApp/views.py
+++ SistemaGarantiaApp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse("Home")
+
+def nosotros(request):
+    return HttpResponse("Nosotros")
+
+def productos(request):
+    return HttpResponse("Productos")
+
+def contacto(request):
+    return HttpResponse("Contacto")
+
+def reclamos(request):
+    return HttpResponse("Reclamos")
