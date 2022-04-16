480f2f38ea8cd3d360b260431ebec87cb4290597
hecdimargimenez <hecdimargimenez@gmail.com>
2022-04-16 16:13:02 -0400

    Migraciones

--- SistemaGarantiaApp/models.py
+++ SistemaGarantiaApp/models.py
@@ -2,24 +2,74 @@ from django.db import models
 
 # Create your models here.
 
-class Ticket(models.Model):
-    cod_ticket=models.CharField(max_length=20, primary_key=True)
-    cod_garantia=models.CharField(max_length=20)
-    cod_departamento=models.CharField(max_length=20)
-    created=models.DateTimeField(auto_now_add=True)
-    updated=models.DateTimeField(auto_now_add=True)
-    tipo_falla=models.CharField(max_length=50)
-    detalle_falla=models.CharField(max_length=300)
-    arch_factura=models.FileField()
-    cert_garantia=models.FileField()
-    estatus=models.BooleanField(default=True)
-
-
 class Cliente(models.Model):
-    cedula=models.CharField(max_length=15, primary_key=True)
-    rif=models.CharField(max_length=15)
-    nombre=models.CharField(max_length=50)
-    apellido=models.CharField(max_length=50)
-    email=models.EmailField(max_length=200)
-    telefono=models.CharField(max_length=20)
+    cedula=models.CharField(max_length=30, primary_key=True)
+    rif=models.CharField(max_length=30)
+    nombre_clte=models.CharField(max_length=255)
+    apellido_clte=models.CharField(max_length=255)
+    email=models.EmailField(max_length=255)
+    telefono=models.CharField(max_length=30)
     estatus=models.BooleanField(default=True)
+
+
+class Factura(models.Model):
+    numero_fact=models.CharField(max_length=30, primary_key=True)
+    cliente_ced=models.ForeignKey(Cliente, null=False, blank=False, on_delete=models.CASCADE)
+    fecha_emision=models.DateTimeField(auto_now_add=True)
+    forma_pago=models.CharField(max_length=255)
+    monto_total=models.IntegerField()
+    estatus=models.BooleanField(default=True)
+
+
+class Garantia(models.Model):
+    numero_garan=models.CharField(max_length=30, primary_key=True)
+    factura_num=models.ForeignKey(Factura, null=False, blank=False, on_delete=models.CASCADE)
+    fecha_inicio=models.DateField()
+    fecha_venc=models.DateField()
+    estatus=models.BooleanField(default=True)
+
+
+class Departamento(models.Model):
+    codigo_dpto=models.CharField(max_length=30, primary_key=True)
+    nombre_dpto=models.CharField(max_length=255)
+    estatus=models.BooleanField(default=True)
+
+
+class Ticket(models.Model):
+    codigo_tick=models.CharField(max_length=30, primary_key=True)
+    garantia_num=models.ForeignKey(Garantia, null=False, blank=False, on_delete=models.CASCADE)
+    departamento_cod=models.ForeignKey(Departamento, null=False, blank=False, on_delete=models.CASCADE)
+    fecha_creac=models.DateTimeField(auto_now_add=True)
+    fecha_act=models.DateTimeField(auto_now_add=True)
+    tipo_falla=models.CharField(max_length=255)
+    detalle_falla=models.CharField(max_length=255)
+    arch_fact=models.FileField()
+    cert_garan=models.FileField()
+    estatus=models.BooleanField(default=True)
+
+
+class Modelo(models.Model):
+   codigo_mdlo=models.CharField(max_length=30, primary_key=True)
+   descrip_mdlo=models.CharField(max_length=255)
+   estatus=models.BooleanField(default=True)
+
+
+class Producto(models.Model):
+    codigo_prod=models.CharField(max_length=30, primary_key=True)
+    modelo_cod=models.ForeignKey(Modelo, null=False, blank=False, on_delete=models.CASCADE)  
+    marca=models.CharField(max_length=255)   
+    nombre_prod=models.CharField(max_length=255)
+    descrip_prod=models.CharField(max_length=255)
+    existencia=models.IntegerField()
+    costo=models.IntegerField()
+    meses_garan=models.IntegerField()
+    estatus=models.BooleanField(default=True)
+
+
+class Detalle(models.Model):
+    codigo_detal=models.CharField(max_length=30, primary_key=True)
+    factura_num=models.ForeignKey(Factura, null=False, blank=False, on_delete=models.CASCADE)
+    producto_cod=models.ForeignKey(Producto, null=False, blank=False, on_delete=models.CASCADE)
+    precio=models.IntegerField()
+    cantidad=models.IntegerField()
+
