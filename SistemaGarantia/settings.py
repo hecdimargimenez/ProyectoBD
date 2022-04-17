dac654aea98cb00ffdafa537625bf584095cd118
hecdimargimenez <hecdimargimenez@gmail.com>
2022-04-17 01:03:39 -0400

    Creando carpeta media

--- SistemaGarantia/settings.py
+++ SistemaGarantia/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os #Importamos os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,3 +124,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Ruta para los archivos de factura y garantia
+MEDIA_URL='/media/'
+
+#Sitio donde debe buscar los archivos media
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+
