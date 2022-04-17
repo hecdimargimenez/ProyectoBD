180e8c878ef957ab6dc8d86cba58d1f1408d0119
hecdimargimenez <hecdimargimenez@gmail.com>
2022-04-16 23:48:08 -0400

    Registrando modelos en admin

--- SistemaGarantiaApp/admin.py
+++ SistemaGarantiaApp/admin.py
@@ -1,11 +1,18 @@
+from msilib.schema import Class
 from django.contrib import admin
-from .models import *
+from .models import * #Importamos todos los modelos 
 
 # Register your models here.
 
+class SistemaGarantiaAdmin(admin.ModelAdmin):
+    readonly_fields=('fecha_creac','fecha_act','fecha_emision')
+
 admin.site.register(Cliente)
-admin.site.register(Cliente)
-admin.site.register(Cliente)
-admin.site.register(Cliente)
-admin.site.register(Cliente)
+admin.site.register(Factura, SistemaGarantiaAdmin)
+admin.site.register(Garantia)
+admin.site.register(Departamento)
+admin.site.register(Ticket,SistemaGarantiaAdmin)
+admin.site.register(Modelo)
+admin.site.register(Producto)
+admin.site.register(Detalle)
 
