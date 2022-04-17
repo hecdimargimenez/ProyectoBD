8863fe5d393a91b23d419ea9d86103bd313bf686
hecdimargimenez <hecdimargimenez@gmail.com>
2022-04-17 00:16:23 -0400

    creando panel de administracion

--- SistemaGarantiaApp/admin.py
+++ SistemaGarantiaApp/admin.py
@@ -1,17 +1,20 @@
-from msilib.schema import Class
 from django.contrib import admin
 from .models import * #Importamos todos los modelos 
 
+#Clases para mostrar los campos en modo lectura
+class TicketAdmin(admin.ModelAdmin):
+    readonly_fields=('fecha_creac','fecha_act')
+
+class FacturaAdmin(admin.ModelAdmin):
+    readonly_fields=('fecha_emision',)
+
 # Register your models here.
 
-class SistemaGarantiaAdmin(admin.ModelAdmin):
-    readonly_fields=('fecha_creac','fecha_act','fecha_emision')
-
 admin.site.register(Cliente)
-admin.site.register(Factura, SistemaGarantiaAdmin)
+admin.site.register(Factura, FacturaAdmin)
 admin.site.register(Garantia)
 admin.site.register(Departamento)
-admin.site.register(Ticket,SistemaGarantiaAdmin)
+admin.site.register(Ticket, TicketAdmin)
 admin.site.register(Modelo)
 admin.site.register(Producto)
 admin.site.register(Detalle)
